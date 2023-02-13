a993b057799a8d597a4b8792f8f935aaa80905d6
Antonov333 <antonovsergei333@yahoo.com>
2023-02-13 22:53:57 +0300

    Домашнее задание №1 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 /* HOMEWORK1 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HOMEWORK1: learning to upload homework...");
+        System.out.println("Привет, мир! Выполняю домашнее задание №1 на курсах Java Developer в SKYPRO");
     }
 }
